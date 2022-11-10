c0185421477b68e5ecc32918b0b17f22c8f7c5e3
isthisthereallife <isthisthereallife@riseup.net>
2022-11-10 17:43:57 +0100

    Big functionality update, see comments.
    Communication with backend activated.
    Search functionality activated.
    Trope import functionality activated.
    Variable grid size activated.
    Hamburger menu activated.
    Bingo detection logic refined.

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
-
+let backendURL = "http://localhost:8080/tropesbingo/"
+let gridSize = 5
 console.log("yeeeah")
 
-document.getElementById("hembrgr").onclick = ((e) => {
-  console.log(e)
-  alert("To Be Implemented")
-})
 const table = document.getElementById("search_results_table")
 const board_div = document.getElementById("board_div")
 let searchField = document.getElementById("search_field")
 let searchForm = document.getElementById("search_form")
+let hembrgr = document.getElementById("hembrgr")
+let hamburgerDiv = document.getElementById("hamburger_div")
 let bingoMsg = document.createElement("span")
-searchField.oninput = ((e) => {
+let gridSizeView = document.getElementById("grid_size")
+let gridSizeLower = document.getElementById("grid_size_lower")
+let gridSizeHigher = document.getElementById("grid_size_higher")
+bingoMsg.setAttribute("id", "bingo_msg")
 
-  populateSearchResults(searchFromUserInput(searchField.value))
+gridSizeHigher.onclick = ((e) => {
+  gridSize++
+  gridSizeView.innerHTML = gridSize
+})
+gridSizeLower.onclick = ((e) => {
+  if (gridSize > 1) {
+    gridSize--
+    gridSizeView.innerHTML = gridSize
+  }
+})
+searchForm.addEventListener("submit", ((e) => {
+  e.preventDefault()
+}))
 
+searchField.oninput = (async (e) => {
+  if (searchField.value.length > 0) {
+    let searchResults = await searchFromUserInput(searchField.value, 1)
+    populateSearchResults(searchResults)
+  } else { table.replaceChildren() }
 })
 
+hembrgr.onclick = ((e) => {
+  if (hembrgr.getAttribute("extended") === "true") {
+    hembrgr.setAttribute("src", "Images/hembrgr.png")
+    hembrgr.setAttribute("extended", "false")
+    hamburgerDiv.style.setProperty("display", "none")
+  }
+  else {
+    hembrgr.setAttribute("extended", "true")
+    hembrgr.setAttribute("src", "Images/hembrgrx.png")
+    hamburgerDiv.style.setProperty("display", "block")
+  }
+})
 
 function populateSearchResults(searchResults) {
-  console.log("populating with data: ", searchResults)
   table.replaceChildren()
   for (item of searchResults) {
-    //console.log("item:", item)
     let a = document.createElement("a")
     a.setAttribute("class", "search_a")
     a.setAttribute("href", `https://tvtropes.org` + item.address)
@@ -59,14 +88,14 @@ function populateSearchResults(searchResults) {
 
     tableDataSelect.appendChild(selectBtn)
 
-    tableDataSelect.onclick = ((e) => {
-      console.log("clicked: ", e.target)
-      console.log(e.target.attributes.link.nodeValue)
-      let tropeList = getTropesFromUrl(e.target.attributes.link.nodeValue)
+    tableDataSelect.onclick = (async (e) => {
+      console.log("suffix-url of chosen item: ", e.target.attributes.link.nodeValue)
+      let tropeList = await getTropesFromUrl(e.target.attributes.link.nodeValue, gridSize)
       /*anropa backend här, med vald films URL (e.target.attributes.link.nodeValue)
-      få tillbaka en lista på 25 objekt med 
+      få tillbaka en lista på 25 objekt med
       title, address, description
       */
+      console.log("tropelist: ", tropeList)
 
 
 
@@ -79,19 +108,16 @@ function populateSearchResults(searchResults) {
       //searchField.style.setProperty("visibility", "hidden")
 
 
-      //gör ett bräde 
+      //gör ett bräde
       const board = document.createElement("table")
-      console.log("made a table")
       board.setAttribute("id", "board")
       let z = 0
-      for (let x = 0; x < 5; x++) {
+      for (let x = 0; x < gridSize; x++) {
         //<tr>
         const tr = document.createElement("tr")
-        console.log("made row ", x)
         //<th class="board_item" id="0x0">Lorem Ipsum</th>
-        for (let y = 0; y < 5; y++) {
+        for (let y = 0; y < gridSize; y++) {
           const td = document.createElement("td")
-          console.log("made a table data entry ", y)
           td.setAttribute("class", "board_item")
           td.setAttribute("id", x + "x" + y)
           td.textContent = tropeList[z].title
@@ -99,49 +125,33 @@ function populateSearchResults(searchResults) {
           td.setAttribute("ticked", "false")
           td.setAttribute("max-width", "18vw")
 
-          console.log(td.textContent)
-          td.setAttribute("title", td.textContent + "\n\n" + tropeList[z].description)
+          td.setAttribute("title", tropeList[z].description)
 
           td.ondblclick = ((e) => {
-            console.log(e)
             window.open(td.getAttribute("address", "_blank"))
           })
           td.onauxclick = ((e) => {
-            console.log(e)
             window.open(td.getAttribute("address", "_blank"))
           })
           td.onclick = ((e) => {
 
-            console.log("trope clicked: ", td.textContent, "ticked?: ", td.getAttribute("ticked"))
-            console.log(td.getAttribute("id"))
-
             if (td.getAttribute("ticked") === "true") {
-              console.log("is now UNticked")
               td.setAttribute("ticked", "false")
               td.style.setProperty("background-color", "white")
               td.style.setProperty("color", "black")
 
             }
             else {
-              console.log("is now ticked")
               td.setAttribute("ticked", "true")
               td.style.setProperty("background-color", "rgb(159, 6, 6)")
               td.style.setProperty("color", "rgb(255, 255, 255)")
             }
             // Check for BINGO here
             if (checkForBingo() === true) {
-              console.log("bingo === true")
               bingoMsg.innerText = "BINGO"
-              bingoMsg.style.setProperty("position", "absolute")
-              bingoMsg.style.setProperty("font-size", "6em")
-              bingoMsg.style.setProperty("background-color", "rgba(0, 191, 255, 0.7)")
-              bingoMsg.style.setProperty("border-radius", "15%")
-              bingoMsg.style.setProperty("user-select", "none")
-              bingoMsg.style.setProperty("pointer-events", "none")
               board_div.append(bingoMsg)
             }
             else {
-              console.log("no more bingo")
               bingoMsg.innerText = ""
             }
 
@@ -174,140 +184,54 @@ function populateSearchResults(searchResults) {
 }
 
 function checkForBingo() {
-  console.log("checking for bingo")
+  let bingo = false
   //horizontal
-  for (let row = 0; row < 5; row++) {
-    for (let cell = 0; cell < 5; cell++) {
+  for (let row = 0; row < gridSize; row++) {
+    for (let cell = 0; cell < gridSize; cell++) {
       let checkHorizontal = document.getElementById(row + "x" + cell)
       if (checkHorizontal.getAttribute("ticked") === "false") { break }
-      if (cell === 4) {
-        console.log("WE HAVE A BINGO HORIZONTALLY")
+      if (cell === gridSize - 1) {
         return true
       }
     }
   }
   //vertical
-  for (let row = 0; row < 5; row++) {
-    for (let cell = 0; cell < 5; cell++) {
+  for (let row = 0; row < gridSize; row++) {
+    for (let cell = 0; cell < gridSize; cell++) {
       let checkVertical = document.getElementById(cell + "x" + row)
       if (checkVertical.getAttribute("ticked") === "false") { break }
-      if (cell === 4) {
-        console.log("WE HAVE A BINGO vertically")
+      if (cell === gridSize - 1) {
         return true
       }
     }
   }
-  //diagonal 0x0 1x1 2x2 3x3 4x4
-  if (document.getElementById("0x0").getAttribute("ticked") === "true" &&
-    document.getElementById("1x1").getAttribute("ticked") === "true" &&
-    document.getElementById("2x2").getAttribute("ticked") === "true" &&
-    document.getElementById("3x3").getAttribute("ticked") === "true" &&
-    document.getElementById("4x4").getAttribute("ticked") === "true") {
-    console.log("DIAGONAL BINGO ltr")
-    return true
+  //diagonal 1
+  for (let i = 0; i < gridSize; i++) {
+    if (document.getElementById(i + "x" + i).getAttribute("ticked") === "false") { break }
+    if (i === gridSize - 1) return true
   }
-  //diagonal 0x4 1x3 2x2 3x1 4x0
-  if (document.getElementById("0x4").getAttribute("ticked") === "true" &&
-    document.getElementById("1x3").getAttribute("ticked") === "true" &&
-    document.getElementById("2x2").getAttribute("ticked") === "true" &&
-    document.getElementById("3x1").getAttribute("ticked") === "true" &&
-    document.getElementById("4x0").getAttribute("ticked") === "true") {
-    console.log("DIAGONAL BINGO rtl")
-    return true
+  //diagonal 2
+  for (let i = 0; i < gridSize; i++) {
+    if (document.getElementById(i + "x" + ((gridSize - 1) - i)).getAttribute("ticked") === "false") { break }
+    if (i === gridSize - 1) return true
   }
 }
 
 
-// TODO set to async + await
-function searchFromUserInput(input) {
-  //jag kommer få in address, title, imgURL, description som JSON
-
-  let backendURL = ""
+async function searchFromUserInput(input, pageNumber) {
   let results = [{}]
-
-  if (backendURL === "") {
-    let mockData = [{
-      address: "/pmwiki/pmwiki.php/Film/TheHobbit", title: "The Hobbit (Film)", imgUrl: "https://static.tvtropes.org/pmwiki/pub/images/e6e70d7d_c031_4d99_8038_7a531a01e186.jpeg",
-      description: `Thorin: So...this is the Hobbit....`
-    }, {
-      address: "/pmwiki/pmwiki.php/WesternAnimation/TheReturnOfTheKing", title: "The Return of the King (WesternAnimation)",
-      imgUrl: "https://static.tvtropes.org/pmwiki/pub/images/return_of_the_king_9.png", description: `Our Orcs Are Different: Than the Goblins in The Hobbit, a case of Art Shift....`
-    }]
-    results = mockData
-  }
-
-
-
-  //det här anropet ska gå till backenden, som i sin tur anropar tvtropes
-  if (input.length > 0 && backendURL !== "") {
-    const res = fetch(backendURL + "/tropesbingo/search/" + input)
-    results = res.then(r => { console.log(r) })
-    res.then(response => { return response.json() })
-  }
-  console.log("returning: ", results)
+  const res = await fetch(backendURL + "search/" + input + "/" + pageNumber)
+  results = res.json()
   return results
 }
 
 
-//TODO set to async + await
-function getTropesFromUrl(url) {
-  let results
-  let backendURL = ""
 
-  if (backendURL === "") {
+async function getTropesFromUrl(url, gridSize = 5) {
+  const results = await fetch(backendURL + gridSize, {
+    method: 'POST',
+    body: url
+  })
 
-    results = [{ title: "Loltrope1", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/NotQuiteDead", description: "trope nummer 1 seru ut såhär" },
-    { title: "tropenummer2", address: "about:blank", description: "tvåannnn" }, {
-      title: "Tropum ipdididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididididisum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "1", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "ä", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "3", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ioooooooooooooooopsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Police Are Useless ", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/PoliceAreUseless", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tropum ipsum", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "o", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "Tm", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "aaaa", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "bbbbb", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "eeeee", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "ppppp", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "eeejjeeeeeeejjm", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/WeirdnessCensor", description: "i got bored"
-    }, {
-      title: "The Only One", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/TheOnlyOne", description: "i got bored"
-    }, {
-      title: "Bystander Syndrome", address: "https://tvtropes.org/pmwiki/pmwiki.php/Main/BystanderSyndrome", description: "i got bored"
-    }]
-  } else {
-    results = fetch(backendURL + "/tropesbingo/film/" + url).then(res => { console.log(res) }).then(res => { return res.json })
-
-
-  }
-  return results
+  return await results.json()
 }
